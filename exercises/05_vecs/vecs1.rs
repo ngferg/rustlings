0b8138e82a22c81e2195325cddf8af3b60c56c62
ngferg <me@nicholaus-ferguson.us>
2025-03-10 23:46:11 -0500

    05_vecs complete

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -1,9 +1,11 @@
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // Array
 
-    // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
-    // Use the vector macro.
-    // let v = ???;
+    let mut v = Vec::new();
+
+    for num in a {
+        v.push(num);
+    }
 
     (a, v)
 }
