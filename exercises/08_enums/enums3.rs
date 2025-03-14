943c4f31188e63469020de35df89cae149940b7c
ngferg <me@nicholaus-ferguson.us>
2025-03-14 01:39:12 -0500

    08_enums complete

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
+        match message {
+            Message::ChangeColor(r,g,b) => self.change_color(r, g, b),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(point) => self.move_position(point),
+            Message::Quit => self.quit(),
+            Message::Resize { width, height } => self.resize(width, height),
+        }
     }
 }
 
