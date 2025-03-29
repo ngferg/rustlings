2c31c85cc09517aee87ebe10413e60d52bf01793
ngferg <me@nicholaus-ferguson.us>
2025-03-28 21:12:36 -0500

    15_traits complete

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -10,8 +10,11 @@ struct OtherSoftware;
 impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+fn compare_license_types<T, U>(software1: T, software2: U) -> bool
+where
+    T: Licensed,
+    U: Licensed
+{
     software1.licensing_info() == software2.licensing_info()
 }
 
